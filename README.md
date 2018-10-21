bdcfe894eff9f52381282db97f24aad8520ca7c6
Hsinheng Hsu <hsin@Hsinhengs-MBP.fios-router.home>
2018-10-21 11:47:40 -0400

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # c_pp_learning
+To compile the program run
+```bash
+$ g++ -std=c++11 hello_world.cpp
+$ ./a.out
+```
+To specify the output filename as `hello`
+```bash
+$ g++ -o hello hello.cpp
+```
+
--- hello_world.cpp
+++ hello_world.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int main()
 {
    //my first c++ program!
-   std::cout << ("Hello World!\n");
+   //std::cout << ("Hello World!\n"); 
+   cout << "hello world" << endl; //works the same as above when you use name space std
+   
+   //variable
+
+   return 0; //you need to have ;
 
 }
